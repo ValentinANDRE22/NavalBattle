fcc3e48d6511d1339be6fba3f26215fa11ab5e6e
RESTECO\valentin.andre <valentin.andre@convivio.fr>
2018-01-08 10:22:47 +0100

    Structure de base

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<?php
+		session_start();
+	include '../includes/all.php';
+
+	?>
+	<title>
+		Bataille Navale
+	</title>
+
+		<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
