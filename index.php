8520af52d82e2dedcf95326f71671c8748b73a02
RESTECO\valentin.andre <valentin.andre@convivio.fr>
2018-01-09 09:23:20 +0100

    Ajout css

--- index.php
+++ index.php
@@ -3,63 +3,73 @@
 	include 'includes/all.php';
 
 	?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
-	<title>
-		Bataille Navale
-	</title>
+		<title>
+			Bataille Navale
+		</title>
 
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<link href="css/style.css" rel="stylesheet"/>
 
-	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+	<br> 
+	<br>
 
-</head>
-<body>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-7">
-			<br> 
-			<form action="index.php" method="POST">
-				<input type="hidden" name="reinit" value="1">
-				<input type="submit" name="reinitialisation" value="Réinitialiser">
-			</form>
-				<?php 
+		<div class="container">
+		<h1>Bataille Navale</h1>
+			<div class="row">
+				<div class="col-md-7">
+
+					<br> 
+					<form action="index.php" method="POST">
+						<input type="hidden" name="reinit" value="1">
+						<input type="submit" name="reinitialisation" value="Réinitialiser" class="btn btn-success">
+					</form>
+					<?php 
 //On test si une partie est déjà en cours.
-				if(isset($_SESSION["newsession"]) && $_SESSION["newsession"]  != true){
-					
-					echo displayGrid($_SESSION["gridPlay"]);
-				} else {
-					initGame();
-					echo displayGrid($_SESSION["gridPlay"]);
-				}
-				?>
+					if(isset($_SESSION["newsession"]) && $_SESSION["newsession"]  != true){
+
+						echo displayGrid($_SESSION["gridPlay"]);
+					} else {
+						initGame();
+						echo displayGrid($_SESSION["gridPlay"]);
+					}
+					?>
+				</div>
+				<div class="col-md-5">
+	<div class="jumbotron">
+						<p>X = Touché.</p>
+						<p>O = A l'eau.</p>
+						<p>C = Coulé.</p>
+					</div>
+					</br>
+					<div class="alert alert-danger"><STRONG>Erreur :</STRONG> Coordonnées incorrecte.</div>
+  					</br>
+					<form action="index.php" method="POST">
+						<label>Colonne</label>
+						<input type="text" name="y"></br>
+						<label>Ligne</label>
+						<input type="text" name="x"></br>
+						<input type="submit" name="valider" value="Valider" class="btn btn-primary">
+					</form>
+
+
+				</div>
+
+
 			</div>
-			<div class="col-md-5">
-
-				<div class="alert alert-danger"><STRONG>Erreur :</STRONG> Coordonnées incorrecte.</div>
-
-				<form action="index.php" method="POST">
-				<label>Colonne</label>
-					<input type="text" name="y"></br>
-				<label>Ligne</label>
-					<input type="text" name="x"></br>
-					<input type="submit" name="valider" value="Valider" class="btn btn-primary">
-				</form>
-
-
-			</div>
-
-
 		</div>
-	</div>
 
 
 
@@ -69,5 +79,5 @@
 
 
 
-</body>
-</html>
+	</body>
+	</html>
